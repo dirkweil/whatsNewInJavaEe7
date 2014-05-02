55caa36368f2920c93e7b83695ec28457ce53942
Dirk Weil <dirk.weil@gedoplan.de>
2014-05-02 09:13:18 +0200

    JavaScript für Websocket-Demo inline

--- src/main/webapp/resources/js/ticker.js
+++
@@ -1,6 +0,0 @@
-var appCtx = window.location.pathname.split('/')[1];
-var webSocket = new WebSocket('ws://' + window.location.host + '/' + appCtx + '/ticker');
-
-webSocket.onmessage = function(event) {
-	document.getElementById('coolNewFeature').value = event.data;
-};
